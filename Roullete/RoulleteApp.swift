73d3cde4399f1d1f2888698ef69108eb9b685454
Macbook pro de Diego <tauildiego@icloud.com>
2025-02-05 15:12:30 -0300

    Initial Commit

---
+++ Roullete/RoulleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoulleteApp.swift
+//  Roullete
+//
+//  Created by Diego Tauil on 05/02/25.
+//
+
+import SwiftUI
+
+@main
+struct RoulleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
